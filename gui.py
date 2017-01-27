bae00a9a644eca7bff1df97cc0572a70c16fe597
Ryan 'GodofGrunts' Whited <dergottdergrunten@gmail.com>
2017-01-27 13:19:12 -0500

    Added scrollbox for scores, changed tab order to be more logical

--- gui.py
+++ gui.py
@@ -68,19 +68,19 @@ class Potassium_OBS_Controller:
         top.configure(highlightcolor="black")
 
 
-        self.Entry1 = Entry(top)
-        self.Entry1.place(relx=0.03, rely=0.18, relheight=0.04, relwidth=0.21)
-        self.Entry1.configure(background="white")
-        self.Entry1.configure(font="TkFixedFont")
-        self.Entry1.configure(selectbackground="#c4c4c4")
-        self.Entry1.configure(textvariable=gui_support.P1_Entry)
+        self.Player1_Name = Entry(top)
+        self.Player1_Name.place(relx=0.03, rely=0.18, relheight=0.04, relwidth=0.21)
+        self.Player1_Name.configure(background="white")
+        self.Player1_Name.configure(font="TkFixedFont")
+        self.Player1_Name.configure(selectbackground="#c4c4c4")
+        self.Player1_Name.configure(textvariable=gui_support.P1_Entry)
 
-        self.Entry1T = Entry(top)
-        self.Entry1T.place(relx=0.03, rely=0.24, relheight=0.04, relwidth=0.21)
-        self.Entry1T.configure(background="#AFEFFF")
-        self.Entry1T.configure(font="TkFixedFont")
-        self.Entry1T.configure(selectbackground="#c4c4c4")
-        self.Entry1T.configure(textvariable=gui_support.P1T_Entry)
+        self.Player1_T = Entry(top)
+        self.Player1_T.place(relx=0.03, rely=0.24, relheight=0.04, relwidth=0.21)
+        self.Player1_T.configure(background="#AFEFFF")
+        self.Player1_T.configure(font="TkFixedFont")
+        self.Player1_T.configure(selectbackground="#c4c4c4")
+        self.Player1_T.configure(textvariable=gui_support.P1T_Entry)
 
         self.Player1_Character = ttk.Combobox(top)
         self.Player1_Character.place(relx=0.03, rely=0.30, relheight=0.04
@@ -90,86 +90,95 @@ class Potassium_OBS_Controller:
         self.Player1_Character.configure(textvariable=gui_support.combobox1)
         self.Player1_Character.configure(takefocus="")
 
-        self.Entry2 = Entry(top)
-        self.Entry2.place(relx=0.26, rely=0.18, relheight=0.04, relwidth=0.21)
-        self.Entry2.configure(background="white")
-        self.Entry2.configure(font="TkFixedFont")
-        self.Entry2.configure(selectbackground="#c4c4c4")
-        self.Entry2.configure(textvariable=gui_support.P2_Entry)
+        self.Player2_Name = Entry(top)
+        self.Player2_Name.place(relx=0.26, rely=0.18, relheight=0.04, relwidth=0.21)
+        self.Player2_Name.configure(background="white")
+        self.Player2_Name.configure(font="TkFixedFont")
+        self.Player2_Name.configure(selectbackground="#c4c4c4")
+        self.Player2_Name.configure(textvariable=gui_support.P2_Entry)
 
-        self.Entry2T = Entry(top)
-        self.Entry2T.place(relx=0.26, rely=0.24, relheight=0.04, relwidth=0.21)
-        self.Entry2T.configure(background="#AFEFFF")
-        self.Entry2T.configure(font="TkFixedFont")
-        self.Entry2T.configure(selectbackground="#c4c4c4")
-        self.Entry2T.configure(textvariable=gui_support.P2T_Entry)
+        self.Player2_T = Entry(top)
+        self.Player2_T.place(relx=0.26, rely=0.24, relheight=0.04, relwidth=0.21)
+        self.Player2_T.configure(background="#AFEFFF")
+        self.Player2_T.configure(font="TkFixedFont")
+        self.Player2_T.configure(selectbackground="#c4c4c4")
+        self.Player2_T.configure(textvariable=gui_support.P2T_Entry)
 
         self.Player2_Character = ttk.Combobox(top)
         self.Player2_Character.place(relx=0.26, rely=0.30, relheight=0.04
                 , relwidth=0.22)
-        self.value_list = ["","Bayonetta","Bowser","Bowser Jr.","Captain Falcon","Charizard","Cloud","Corrin","Dark Pit","Diddy Kong","Donkey Kong","Dr. Mario","Duck Hunt","Falco","Fox","Ganondorf","Greninja","Ike","Jigglypuff","King Dedede","Kirby","Link","Little Mac","Lucario","Lucas","Lucina","Luigi","Mario","Marth","Mega Man","Meta Knight","Mewtwo","Mii Brawler","Mii Gunner","Mii Swordfighter","Mr. Game & Watch","Ness","Olimar","Pac-Man","Palutena","Peach","Pikachu","Pit","R.O.B.","Robin","Rosalina & Luma","Roy","Ryu","Samus","Sheik","Shulk","Sonic","Toon Link","Villager","Wario","Wii Fit Trainer","Yoshi","Zelda","Zero Suit Samus"]
+        #self.value_list = ["","Bayonetta","Bowser","Bowser Jr.","Captain Falcon","Charizard","Cloud","Corrin","Dark Pit","Diddy Kong","Donkey Kong","Dr. Mario","Duck Hunt","Falco","Fox","Ganondorf","Greninja","Ike","Jigglypuff","King Dedede","Kirby","Link","Little Mac","Lucario","Lucas","Lucina","Luigi","Mario","Marth","Mega Man","Meta Knight","Mewtwo","Mii Brawler","Mii Gunner","Mii Swordfighter","Mr. Game & Watch","Ness","Olimar","Pac-Man","Palutena","Peach","Pikachu","Pit","R.O.B.","Robin","Rosalina & Luma","Roy","Ryu","Samus","Sheik","Shulk","Sonic","Toon Link","Villager","Wario","Wii Fit Trainer","Yoshi","Zelda","Zero Suit Samus"]
         self.Player2_Character.configure(values=self.value_list)
         self.Player2_Character.configure(textvariable=gui_support.combobox2)
         self.Player2_Character.configure(takefocus="")
 
-        self.Entry3 = Entry(top)
-        self.Entry3.place(relx=0.5, rely=0.18, relheight=0.04, relwidth=0.21)
-        self.Entry3.configure(background="white")
-        self.Entry3.configure(font="TkFixedFont")
-        self.Entry3.configure(selectbackground="#c4c4c4")
-        self.Entry3.configure(textvariable=gui_support.P3_Entry)
+        self.Player3_Name = Entry(top)
+        self.Player3_Name.place(relx=0.5, rely=0.18, relheight=0.04, relwidth=0.21)
+        self.Player3_Name.configure(background="white")
+        self.Player3_Name.configure(font="TkFixedFont")
+        self.Player3_Name.configure(selectbackground="#c4c4c4")
+        self.Player3_Name.configure(textvariable=gui_support.P3_Entry)
 
-        self.Entry3T = Entry(top)
-        self.Entry3T.place(relx=0.5, rely=0.24, relheight=0.04, relwidth=0.21)
-        self.Entry3T.configure(background="#AFEFFF")
-        self.Entry3T.configure(font="TkFixedFont")
-        self.Entry3T.configure(selectbackground="#c4c4c4")
-        self.Entry3T.configure(textvariable=gui_support.P3T_Entry)
+        self.Player3_T = Entry(top)
+        self.Player3_T.place(relx=0.5, rely=0.24, relheight=0.04, relwidth=0.21)
+        self.Player3_T.configure(background="#AFEFFF")
+        self.Player3_T.configure(font="TkFixedFont")
+        self.Player3_T.configure(selectbackground="#c4c4c4")
+        self.Player3_T.configure(textvariable=gui_support.P3T_Entry)
 
         self.Player3_Character = ttk.Combobox(top)
         self.Player3_Character.place(relx=0.5, rely=0.30, relheight=0.04
                 , relwidth=0.22)
-        self.value_list = ["","Bayonetta","Bowser","Bowser Jr.","Captain Falcon","Charizard","Cloud","Corrin","Dark Pit","Diddy Kong","Donkey Kong","Dr. Mario","Duck Hunt","Falco","Fox","Ganondorf","Greninja","Ike","Jigglypuff","King Dedede","Kirby","Link","Little Mac","Lucario","Lucas","Lucina","Luigi","Mario","Marth","Mega Man","Meta Knight","Mewtwo","Mii Brawler","Mii Gunner","Mii Swordfighter","Mr. Game & Watch","Ness","Olimar","Pac-Man","Palutena","Peach","Pikachu","Pit","R.O.B.","Robin","Rosalina & Luma","Roy","Ryu","Samus","Sheik","Shulk","Sonic","Toon Link","Villager","Wario","Wii Fit Trainer","Yoshi","Zelda","Zero Suit Samus"]
+        #self.value_list = ["","Bayonetta","Bowser","Bowser Jr.","Captain Falcon","Charizard","Cloud","Corrin","Dark Pit","Diddy Kong","Donkey Kong","Dr. Mario","Duck Hunt","Falco","Fox","Ganondorf","Greninja","Ike","Jigglypuff","King Dedede","Kirby","Link","Little Mac","Lucario","Lucas","Lucina","Luigi","Mario","Marth","Mega Man","Meta Knight","Mewtwo","Mii Brawler","Mii Gunner","Mii Swordfighter","Mr. Game & Watch","Ness","Olimar","Pac-Man","Palutena","Peach","Pikachu","Pit","R.O.B.","Robin","Rosalina & Luma","Roy","Ryu","Samus","Sheik","Shulk","Sonic","Toon Link","Villager","Wario","Wii Fit Trainer","Yoshi","Zelda","Zero Suit Samus"]
         self.Player3_Character.configure(values=self.value_list)
         self.Player3_Character.configure(textvariable=gui_support.combobox3)
         self.Player3_Character.configure(takefocus="")
 
-        self.Entry4 = Entry(top)
-        self.Entry4.place(relx=0.75, rely=0.18, relheight=0.04, relwidth=0.21)
-        self.Entry4.configure(background="white")
-        self.Entry4.configure(font="TkFixedFont")
-        self.Entry4.configure(selectbackground="#c4c4c4")
-        self.Entry4.configure(textvariable=gui_support.P4_Entry)
+        self.Player4_Name = Entry(top)
+        self.Player4_Name.place(relx=0.75, rely=0.18, relheight=0.04, relwidth=0.21)
+        self.Player4_Name.configure(background="white")
+        self.Player4_Name.configure(font="TkFixedFont")
+        self.Player4_Name.configure(selectbackground="#c4c4c4")
+        self.Player4_Name.configure(textvariable=gui_support.P4_Entry)
 
-        self.Entry4T = Entry(top)
-        self.Entry4T.place(relx=0.75, rely=0.24, relheight=0.04, relwidth=0.21)
-        self.Entry4T.configure(background="#AFEFFF")
-        self.Entry4T.configure(font="TkFixedFont")
-        self.Entry4T.configure(selectbackground="#c4c4c4")
-        self.Entry4T.configure(textvariable=gui_support.P4T_Entry)
+        self.Player4_T = Entry(top)
+        self.Player4_T.place(relx=0.75, rely=0.24, relheight=0.04, relwidth=0.21)
+        self.Player4_T.configure(background="#AFEFFF")
+        self.Player4_T.configure(font="TkFixedFont")
+        self.Player4_T.configure(selectbackground="#c4c4c4")
+        self.Player4_T.configure(textvariable=gui_support.P4T_Entry)
 
         self.Player4_Character = ttk.Combobox(top)
         self.Player4_Character.place(relx=0.75, rely=0.30, relheight=0.04
                 , relwidth=0.22)
-        self.value_list = ["","Bayonetta","Bowser","Bowser Jr.","Captain Falcon","Charizard","Cloud","Corrin","Dark Pit","Diddy Kong","Donkey Kong","Dr. Mario","Duck Hunt","Falco","Fox","Ganondorf","Greninja","Ike","Jigglypuff","King Dedede","Kirby","Link","Little Mac","Lucario","Lucas","Lucina","Luigi","Mario","Marth","Mega Man","Meta Knight","Mewtwo","Mii Brawler","Mii Gunner","Mii Swordfighter","Mr. Game & Watch","Ness","Olimar","Pac-Man","Palutena","Peach","Pikachu","Pit","R.O.B.","Robin","Rosalina & Luma","Roy","Ryu","Samus","Sheik","Shulk","Sonic","Toon Link","Villager","Wario","Wii Fit Trainer","Yoshi","Zelda","Zero Suit Samus"]
+        #self.value_list = ["","Bayonetta","Bowser","Bowser Jr.","Captain Falcon","Charizard","Cloud","Corrin","Dark Pit","Diddy Kong","Donkey Kong","Dr. Mario","Duck Hunt","Falco","Fox","Ganondorf","Greninja","Ike","Jigglypuff","King Dedede","Kirby","Link","Little Mac","Lucario","Lucas","Lucina","Luigi","Mario","Marth","Mega Man","Meta Knight","Mewtwo","Mii Brawler","Mii Gunner","Mii Swordfighter","Mr. Game & Watch","Ness","Olimar","Pac-Man","Palutena","Peach","Pikachu","Pit","R.O.B.","Robin","Rosalina & Luma","Roy","Ryu","Samus","Sheik","Shulk","Sonic","Toon Link","Villager","Wario","Wii Fit Trainer","Yoshi","Zelda","Zero Suit Samus"]
         self.Player4_Character.configure(values=self.value_list)
         self.Player4_Character.configure(textvariable=gui_support.combobox4)
         self.Player4_Character.configure(takefocus="")
 
-        self.Player1_Wins = Entry(top)
-        self.Player1_Wins.place(relx=0.15, rely=0.4, relheight=0.04
+        self.Player1_Wins = Spinbox(top, from_=0.0, to=100.0)
+        self.Player1_Wins.place(relx=0.15, rely=0.4, relheight=0.06
                 , relwidth=0.21)
+        self.Player1_Wins.configure(activebackground="#f9f9f9")
         self.Player1_Wins.configure(background="white")
-        self.Player1_Wins.configure(font="TkFixedFont")
+        self.Player1_Wins.configure(from_="0.0")
+        self.Player1_Wins.configure(highlightbackground="black")
         self.Player1_Wins.configure(selectbackground="#c4c4c4")
         self.Player1_Wins.configure(textvariable=gui_support.P1_Wins)
+        self.Player1_Wins.configure(to="100.0")
+        self.Player1_Wins.configure(width=98)
 
-        self.Entry8 = Entry(top)
-        self.Entry8.place(relx=0.63, rely=0.4, relheight=0.04, relwidth=0.21)
-        self.Entry8.configure(background="white")
-        self.Entry8.configure(font="TkFixedFont")
-        self.Entry8.configure(selectbackground="#c4c4c4")
-        self.Entry8.configure(textvariable=gui_support.P2_Wins)
+        self.Player2_Wins = Spinbox(top, from_=0.0, to=100.0)
+        self.Player2_Wins.place(relx=0.63, rely=0.4, relheight=0.06
+                , relwidth=0.21)
+        self.Player2_Wins.configure(activebackground="#f9f9f9")
+        self.Player2_Wins.configure(background="white")
+        self.Player2_Wins.configure(from_="0.0")
+        self.Player2_Wins.configure(highlightbackground="black")
+        self.Player2_Wins.configure(selectbackground="#c4c4c4")
+        self.Player2_Wins.configure(textvariable=gui_support.P2_Wins)
+        self.Player2_Wins.configure(to="100.0")
+        self.Player2_Wins.configure(width=98)
 
         self.Com1 = Entry(top)
         self.Com1.place(relx=0.15, rely=0.53, relheight=0.04, relwidth=0.21)
@@ -185,12 +194,12 @@ class Potassium_OBS_Controller:
         self.Com1T.configure(selectbackground="#c4c4c4")
         self.Com1T.configure(textvariable=gui_support.Com1T_Entry)
 
-        self.Entry10 = Entry(top)
-        self.Entry10.place(relx=0.63, rely=0.53, relheight=0.04, relwidth=0.21)
-        self.Entry10.configure(background="white")
-        self.Entry10.configure(font="TkFixedFont")
-        self.Entry10.configure(selectbackground="#c4c4c4")
-        self.Entry10.configure(textvariable=gui_support.Com2_Entry)
+        self.Com2 = Entry(top)
+        self.Com2.place(relx=0.63, rely=0.53, relheight=0.04, relwidth=0.21)
+        self.Com2.configure(background="white")
+        self.Com2.configure(font="TkFixedFont")
+        self.Com2.configure(selectbackground="#c4c4c4")
+        self.Com2.configure(textvariable=gui_support.Com2_Entry)
 
         self.Com2T = Entry(top)
         self.Com2T.place(relx=0.63, rely=0.59, relheight=0.04, relwidth=0.21)
@@ -198,13 +207,6 @@ class Potassium_OBS_Controller:
         self.Com2T.configure(font="TkFixedFont")
         self.Com2T.configure(selectbackground="#c4c4c4")
         self.Com2T.configure(textvariable=gui_support.Com2T_Entry)
-
-        self.Round = Entry(top)
-        self.Round.place(relx=0.5, rely=0.07, relheight=0.04, relwidth=0.21)
-        self.Round.configure(background="white")
-        self.Round.configure(font="TkFixedFont")
-        self.Round.configure(selectbackground="#c4c4c4")
-        self.Round.configure(textvariable=gui_support.Round_Entry)
 
         self.Save = Button(top)
         self.Save.place(relx=0.04, rely=0.67, height=26, width=121)
@@ -223,6 +225,37 @@ class Potassium_OBS_Controller:
         self.Stop.configure(activebackground="#d9d9d9")
         self.Stop.configure(command=gui_support.stop_recording)
         self.Stop.configure(text='''Stop Recording''')
+
+        self.Event_Text = Entry(top)
+        self.Event_Text.place(relx=0.03, rely=0.07, relheight=0.04
+                , relwidth=0.21)
+        self.Event_Text.configure(background="white")
+        self.Event_Text.configure(font="TkFixedFont")
+        self.Event_Text.configure(selectbackground="#c4c4c4")
+        self.Event_Text.configure(textvariable=gui_support.Event_Entry)
+
+        self.EventSH_Text = Entry(top)
+        self.EventSH_Text.place(relx=0.26, rely=0.07, relheight=0.04
+                , relwidth=0.21)
+        self.EventSH_Text.configure(background="white")
+        self.EventSH_Text.configure(font="TkFixedFont")
+        self.EventSH_Text.configure(selectbackground="#c4c4c4")
+        self.EventSH_Text.configure(textvariable=gui_support.EventSH_Entry)
+
+        self.Round = Entry(top)
+        self.Round.place(relx=0.5, rely=0.07, relheight=0.04, relwidth=0.21)
+        self.Round.configure(background="white")
+        self.Round.configure(font="TkFixedFont")
+        self.Round.configure(selectbackground="#c4c4c4")
+        self.Round.configure(textvariable=gui_support.Round_Entry)
+
+        self.Game_Box = ttk.Combobox(top)
+        self.Game_Box.place(relx=0.75, rely=0.07, relheight=0.04
+                , relwidth=0.22)
+        self.game_list = ["64", "Melee", "Brawl", "PM", "Wii U", "Other"]
+        self.Game_Box.configure(values=self.game_list)
+        self.Game_Box.configure(textvariable=gui_support.game)
+        self.Game_Box.configure(takefocus="")
 
 ###############################################################################
 ################## Above this is the order for tabbing ########################
@@ -247,30 +280,6 @@ class Potassium_OBS_Controller:
         self.Player4.place(relx=0.82, rely=0.13, height=18, width=52)
         self.Player4.configure(activebackground="#f9f9f9")
         self.Player4.configure(text='''Player 4''')
-
-        self.Event_Text = Entry(top)
-        self.Event_Text.place(relx=0.03, rely=0.07, relheight=0.04
-                , relwidth=0.21)
-        self.Event_Text.configure(background="white")
-        self.Event_Text.configure(font="TkFixedFont")
-        self.Event_Text.configure(selectbackground="#c4c4c4")
-        self.Event_Text.configure(textvariable=gui_support.Event_Entry)
-
-        self.EventSH_Text = Entry(top)
-        self.EventSH_Text.place(relx=0.26, rely=0.07, relheight=0.04
-                , relwidth=0.21)
-        self.EventSH_Text.configure(background="white")
-        self.EventSH_Text.configure(font="TkFixedFont")
-        self.EventSH_Text.configure(selectbackground="#c4c4c4")
-        self.EventSH_Text.configure(textvariable=gui_support.EventSH_Entry)
-
-        self.Game_Box = ttk.Combobox(top)
-        self.Game_Box.place(relx=0.75, rely=0.07, relheight=0.04
-                , relwidth=0.22)
-        self.game_list = ["64", "Melee", "Brawl", "PM", "Wii U", "Other"]
-        self.Game_Box.configure(values=self.game_list)
-        self.Game_Box.configure(textvariable=gui_support.game)
-        self.Game_Box.configure(takefocus="")
 
         self.Event1 = Label(top)
         self.Event1.place(relx=0.087, rely=0.02, height=18, width=78)
